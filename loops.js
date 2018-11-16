10a84e26ccef0154ca5eb56d8d0d51a8fcd870dd
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-16 19:43:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(array){
   for (var i = 0; i <= 25; i++){
     if (i == 1) {
-      array = ["I am " + i + " strange loop.\n"]
+      array = ["I am " + i + " strange loop."]
     } else {
-      array += ["I am " + i + " strange loops.\n"]
+      array += ["I am " + i + " strange loops."]
     }
   }
 }
