8fc1a6af8753c23050316a5ded37bfce688c55ee
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-16 19:37:23 +0000

    automatically backed up by learn

---
+++ mytest.js
@@ -0,0 +1,13 @@
+function forLoop(){
+  var someArray = []
+  for (var i = 0; i <= 25; i++){
+    if (i == 1) {
+    someArray = ["I am ${i} strange loop."]
+    } else {
+      someArray += ["I am ${i} strange loops."]
+    }
+    console.log(someArray)
+  }
+}
+
+forLoop()
