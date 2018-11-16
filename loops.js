49961f84b1e035d35c0f908e4da494af96095d6d
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-16 19:48:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -9,6 +9,15 @@ function forLoop(array){
   return array
 }
 
+function whileLoop(n){
+  while (n > 0) {
+    n--
+    console.log(n)
+  }
+  return 'done'
+  
+}
+
 function doWhileLoop(array) {
   
 }
